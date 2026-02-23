51fe519689dd0e88a9a5234239f5f73ebddf3125
AnnaVitkina <anna_vitkina@trimble.com>
2026-02-23 19:38:10 +0200

    Update pipeline_main.py

--- pipeline_main.py
+++ pipeline_main.py
@@ -21,7 +21,7 @@ HARDCODED_INPUT_FOLDER = "/content/drive/Shareddrives/FA Ops Europe: Rate Mainte
 HARDCODED_ARCHIVE_FOLDER = "/content/drive/Shareddrives/FA Ops Europe: Rate Maintenance Team /Documents/AI Adoption RMT/RMT/archive"
 HARDCODED_CLIENTS_FILE = "/content/drive/Shareddrives/FA Ops Europe: Rate Maintenance Team /Documents/AI Adoption RMT/RMT/addition/clients.txt"
 HARDCODED_COUNTRY_CODES_FILE = "/content/drive/Shareddrives/FA Ops Europe: Rate Maintenance Team /Documents/AI Adoption RMT/RMT/addition/dhl_country_codes.txt"
-HARDCODED_ACCESSORIAL_FILE = "/content/drive/Shareddrives/FA Ops Europe: Rate Maintenance Team /Documents/AI Adoption RMT/RMT/addition/Accessorial Costs.xlsx"
+#HARDCODED_ACCESSORIAL_FILE = "/content/drive/Shareddrives/FA Ops Europe: Rate Maintenance Team /Documents/AI Adoption RMT/RMT/addition/Accessorial Costs.xlsx"
 HARDCODED_OUTPUT_DIR = "/content/drive/Shareddrives/FA Ops Europe: Rate Maintenance Team /Documents/AI Adoption RMT/RMT/output"
 
 # Local (Windows) – used when Drive path does not exist
@@ -473,6 +473,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
